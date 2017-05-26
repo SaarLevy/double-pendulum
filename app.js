cd887c66bf05a82801a08b08725fc88512a16899
Saar Levy <saar.levy0@gmail.com>
2017-05-26 22:56:11 +0300

    Moved anchor point to the center

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-const width  = 600,
-      height = 600;
+'use strict'
+
+const w = 600,
+      h = 600;
 
 // module aliases
 const Engine = Matter.Engine,
       Render = Matter.Render,
       World = Matter.World,
-      Bodies = Matter.Bodies;
-      Constraint = Matter.Constraint;
-      MouseConstraint = Matter.MouseConstraint;
+      Bodies = Matter.Bodies,
+      Constraint = Matter.Constraint,
+      MouseConstraint = Matter.MouseConstraint,
       Mouse = Matter.Mouse;
 
+const stageElement = $('#stage')[0];
 
 // matterjs boilerplate: Engine, renderer and world
-const engine = Engine.create();
+const engine = Engine.create({
+    gravity: 1
+});
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: width,
-        height: height
+        width: w,
+        height: h
     }
 });
 const world = engine.world
 const mouse = Mouse.create(render.canvas)
 
 // Bodies and constraints
-const center = Bodies.circle(height/2, width/2, 5, {isStatic: true});
-const ballA = Bodies.circle(height/2, 100, 20, {density: 50});
-const ballB = Bodies.circle(height/2, 300, 20, {density: 100});
+const center = Bodies.circle(h/2, w/2, 0, {isStatic: true});
+const ballA = Bodies.circle(h/2, 100, 20, {density: 50});
+const ballB = Bodies.circle(h/2, 300, 20, {density: 50});
 
 const A2Center = Constraint.create({bodyA: center,
-                                  bodyB: ballA,
-                                  length: 200});
+                                    bodyB: ballA,
+                                    length: 100,
+                                    stiffness: 1});
 
 const A2B  = Constraint.create({bodyA: ballA,
                                 bodyB: ballB,
-                                length: 100})
+                                length: 100,
+                                stiffness: 1})
 const mouseConstraint = MouseConstraint.create(engine, {
     mouse: mouse,
     constraint: {
